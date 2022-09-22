56cf535283a40fa3285aa7ab2f5236b143843da2
Zoltán Király <zoliky@gmail.com>
2022-09-22 14:17:57 +0300

    Sync

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home, snippetView, and snippetCreate functions as handlers
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
+	// Use the http.ListenAndServe() function to start a new web server.
+	// If http.ListenAndServe() returns an error, use the log.Fatal() function
+	// to log the error message and exit
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
