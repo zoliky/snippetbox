705f90217d5d3c3df30f363f6d2b95493fa733df
Zoltán Király <zoliky@gmail.com>
2022-09-22 14:12:48 +0300

    Sync

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -7,15 +7,40 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Check if the current request URL path exactly matches "/". If it doesn't,
+	// use the http.NotFound() function to send a 404 response to the client
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	w.Write([]byte("Hello from Snippetbox"))
+	// Use the template.ParseFiles() function to read the template files
+	// into a template set. If there's an error, log the detailed error
+	// error message and use the http.Error() function to send a generic
+	// 500 Internal Server Error response to the user
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// Use the Execute() method on the template set to write the template
+	// content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in to the template,
+	// for now nothing (nil)
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	// Extract the value of the id parameter from the query string and try to
+	// convert it to an integer using the strconv.Atoi() function. If it can't
+	// be converted to an integer, or the value is less than 1, return a
+	// 404 page not found response
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -26,6 +51,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
