c7dd8ee0026f518df18090277f9760f08e22d16e
Zoltán Király <zoliky@gmail.com>
2022-09-23 12:50:49 +0300

    Sync

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,16 @@ func main() {
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home, snippetView, and snippetCreate functions as handlers
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	// Register the other application routes as normal
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
