71f4ca3226b4a455b6dd0603ab9a8d1d0a830217
Zoltán Király <zoliky@gmail.com>
2022-09-23 12:19:28 +0300

    Sync

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -16,22 +16,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the template.ParseFiles() function to read the template files
-	// into a template set. If there's an error, log the detailed error
-	// error message and use the http.Error() function to send a generic
-	// 500 Internal Server Error response to the user
-	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	// Initialize a slice containing the paths to the two files. It's important
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	
+	// Use the template.ParseFiles() function to read the files and store
+	// the templates in a template set. Pass the slice of file paths as a
+	// variadic parameter
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 		return
 	}
 
-	// Use the Execute() method on the template set to write the template
-	// content as the response body. The last parameter to Execute()
-	// represents any dynamic data that we want to pass in to the template,
-	// for now nothing (nil)
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
